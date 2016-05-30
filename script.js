06a80b524dd493399d43fd28b2c5321640b64216
John Doe <webdevnorth@gmail.com>
2016-05-30 15:14:51 +0100

    added card matching functionality

--- script.js
+++ script.js
@@ -13,3 +13,53 @@ $('#randomizer').click(function() {
 	}
 
 }); // randomly assign colours to back face of cards
+
+
+var bgValue = undefined; // storage var for background prop value of first card clicked
+var removed = []; // storage arr for matched cards
+
+$('.card').click(function() {
+
+	$('.card').css('pointer-events', 'none'); // disable clicks on cards	
+	$(this).addClass('flipped'); // flip card	
+
+	// if 2nd card clicked, check if its background prop value matches first card's bgValue
+	// if 1st card clicked, store its background prop value in bgValue		
+	if (bgValue !== undefined) {
+		// if cards match, remove from play
+		// else flip them back over
+		if (bgValue === $(this).find('.back-face').css('background-color')) {
+			setTimeout(function() {
+				$('.flipped').fadeOut(850);
+			}, 600);
+			
+			setTimeout(function() {
+				$('.flipped').find('.back-face').each(function(index, element) {
+					removed.push(element.id);
+				}); // push IDs for both matched cards to removed array
+				console.log(removed); // TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST 
+				$('.flipped').removeClass('flipped');
+				$('.card').css('pointer-events', 'auto');
+
+				if (removed.length === 12) {
+					// Game has been won.
+					console.log("Game over");
+				}
+			}, 1550);			
+		} else {
+			setTimeout(function() {
+				$('.flipped').removeClass('flipped');
+				$('.card').css('pointer-events', 'auto'); 
+			}, 900)
+		}
+		
+		bgValue = undefined;
+	} else {
+		bgValue = $(this).find('.back-face').css('background-color');
+		console.log($(this).find('.back-face').css('background-color'));
+		setTimeout(function() {
+			$('.card').css('pointer-events', 'auto'); 
+		}, 500); // restore clickable state to cards after 0.5s (in line w/ transition duration)
+	}
+
+}); // end card.click
