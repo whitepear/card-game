5efde28a09eec7e4c2b2612829c5bc352c1fd686
John Doe <webdevnorth@gmail.com>
2016-05-31 15:59:25 +0100

    added modal window

--- script.js
+++ script.js
@@ -1,65 +1,85 @@
-$('#randomizer').click(function() {
-	var colours = ['red', 'yellow', 'blue', 'green', 'lime', 'orange', 'red', 'yellow', 'blue', 'green', 'lime', 'orange'];
-	var cards = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve'];
+//  INCLUDE CHECK FOR ID TO PREVENT USER CLICKING SAME CARD TWICE TO "MATCH"
 
-	function getRandomIntInclusive(min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	} // generates a random number inclusive of max value
+$(document).ready(function() {
 
-	for (var i = 0; i < cards.length; i++) {
-		var randomColour = getRandomIntInclusive(0, colours.length-1);
-		$('#' + cards[i]).css('background', colours[randomColour]); // assign colour to card
-		colours.splice(randomColour, 1); // remove colour from array
-	}
+	randomFaces();	
 
-}); // randomly assign colours to back face of cards
+	function randomFaces () {
+		var colours = ['pink', 'yellow', 'blue', 'green', 'lime', 'orange', 'pink', 'yellow', 'blue', 'green', 'lime', 'orange'];
+		var cards = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve'];
 
+		function getRandomIntInclusive(min, max) {
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		} // generates a random number inclusive of max value
 
-var bgValue = undefined; // storage var for background prop value of first card clicked
-var removed = []; // storage arr for matched cards
-
-$('.card').click(function() {
-
-	$('.card').css('pointer-events', 'none'); // disable clicks on cards	
-	$(this).addClass('flipped'); // flip card	
-
-	// if 2nd card clicked, check if its background prop value matches first card's bgValue
-	// if 1st card clicked, store its background prop value in bgValue		
-	if (bgValue !== undefined) {
-		// if cards match, remove from play
-		// else flip them back over
-		if (bgValue === $(this).find('.back-face').css('background-color')) {
-			setTimeout(function() {
-				$('.flipped').fadeOut(850);
-			}, 600);
-			
-			setTimeout(function() {
-				$('.flipped').find('.back-face').each(function(index, element) {
-					removed.push(element.id);
-				}); // push IDs for both matched cards to removed array
-				console.log(removed); // TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST 
-				$('.flipped').removeClass('flipped');
-				$('.card').css('pointer-events', 'auto');
-
-				if (removed.length === 12) {
-					// Game has been won.
-					console.log("Game over");
-				}
-			}, 1550);			
-		} else {
-			setTimeout(function() {
-				$('.flipped').removeClass('flipped');
-				$('.card').css('pointer-events', 'auto'); 
-			}, 900)
+		for (var i = 0; i < cards.length; i++) {
+			var randomColour = getRandomIntInclusive(0, colours.length-1);
+			$('#' + cards[i]).css('background', colours[randomColour]); // assign colour to card
+			colours.splice(randomColour, 1); // remove colour from array
 		}
-		
-		bgValue = undefined;
-	} else {
-		bgValue = $(this).find('.back-face').css('background-color');
-		console.log($(this).find('.back-face').css('background-color'));
-		setTimeout(function() {
-			$('.card').css('pointer-events', 'auto'); 
-		}, 500); // restore clickable state to cards after 0.5s (in line w/ transition duration)
-	}
 
-}); // end card.click
+	} // randomly assign colours to back face of cards
+
+
+	var bgValue = undefined; // storage var for background prop value of first card clicked
+	var firstID = undefined; // storage var for id value of first card clicked
+	var removed = []; // storage arr for matched cards
+
+	$('.card').click(function() {
+
+		$('.card').css('pointer-events', 'none'); // disable clicks on cards	
+		$(this).addClass('flipped'); // flip card	
+
+		// if 2nd card clicked, check if its background prop value matches first card's bgValue
+		// if 1st card clicked, store its background prop value in bgValue		
+		if (bgValue !== undefined) {
+			// if cards match, remove from play
+			// else flip them back over
+			if (bgValue === $(this).find('.back-face').css('background-color')) {
+				setTimeout(function() {
+					$('.flipped').fadeOut(850);
+				}, 600);
+				
+				setTimeout(function() {
+					$('.flipped').find('.back-face').each(function(index, element) {
+						removed.push(element.id);
+					}); // push IDs for both matched cards to removed array
+					console.log(removed); // TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST TEST 
+					$('.flipped').removeClass('flipped');
+					$('.card').css('pointer-events', 'auto');
+
+					if (removed.length === 12) {
+						modalToggle();
+					}
+				}, 1550);			
+			} else {
+				setTimeout(function() {
+					$('.flipped').removeClass('flipped');
+					$('.card').css('pointer-events', 'auto'); 
+				}, 900);
+			}
+			
+			bgValue = undefined;
+		} else {
+			bgValue = $(this).find('.back-face').css('background-color');
+			console.log($(this).find('.back-face').css('background-color'));
+			setTimeout(function() {
+				$('.card').css('pointer-events', 'auto'); 
+			}, 500); // restore clickable state to cards after 0.5s (in line w/ transition duration)
+		}
+
+	}); // end card.click
+
+	function modalToggle () {
+		if ($('#overlay').css('visibility') === 'hidden') {
+			$('#overlay').css('visibility', 'visible').css('background', 'rgba(0, 0, 0, 0.5)');
+			$('.modal-content').addClass('modal-active');
+		} else {
+			$('#overlay').css('visibility', 'hidden').css('background', 'rgba(0, 0, 0, 0)');
+			$('.modal-content').removeClass('modal-active');
+		}
+	} // end modalToggle
+
+	$('#close-modal').click(modalToggle);
+
+}); // end .ready
